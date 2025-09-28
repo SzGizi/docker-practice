e25e1c02617d538481ce80d37075f14adc1ade34
Gizi <szanto.gizela@gmail.com>
2025-09-28 17:12:19 +0200

    practice optimizing images

--- optimizing-images/index.js
+++ optimizing-images/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  return res.send('Hello from express!');
+});
+//console.log("Hello, World!");
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
