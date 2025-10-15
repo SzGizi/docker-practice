f031d31219f54b365b0c1459d16767de5ec4e805
Gizi <szanto.gizela@gmail.com>
2025-10-15 09:43:18 +0200

    Volumes and Networks in Docker Compose

---
+++ compose/db-config/mongo-init.js
@@ -0,0 +1,14 @@
+const keyValueDb = process.env.KEY_VALUE_DB;
+const keyValueUser = process.env.KEY_VALUE_USER;
+const keyValuePassword = process.env.KEY_VALUE_PASSWORD;
+
+console.log("Init: key value db user");
+db = db.getSiblingDB(keyValueDb);
+
+db.createUser({
+  user: keyValueUser,
+  pwd: keyValuePassword,
+  roles: [{ 
+    role: 'readWrite', 
+    db: keyValueDb }],
+});
