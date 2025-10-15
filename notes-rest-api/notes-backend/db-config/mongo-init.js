15ce97305256c4d5afb68e01b88c065649fbb9c0
Gizi <szanto.gizela@gmail.com>
2025-10-15 12:40:33 +0200

    Setup Docker Compose for Notes Services

---
+++ notes-rest-api/notes-backend/db-config/mongo-init.js
@@ -0,0 +1,14 @@
+const notesDbName = process.env.NOTES_DB_NAME;
+const notesDbUser = process.env.NOTES_DB_USER;
+const notesDbPassword = process.env.NOTES_DB_PASSWORD;
+
+console.log("Init:notes init user");
+db = db.getSiblingDB(notesDbName);
+
+db.createUser({
+  user: notesDbUser,
+  pwd: notesDbPassword,
+  roles: [{ 
+    role: 'readWrite', 
+    db: notesDbName }],
+});
--- notes-rest-api/notes-backend/src/server.js
+++ notes-rest-api/notes-backend/src/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/',(req,res)=> res.json({message: 'hello from notes'}));
 
-app.listen(3001,()=>{
-    console.log("notes Server lisening 3001 ");
+const port = process.env.PORT;
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("Connected to MOngoDB! Start server");
+
+    app.listen(port,()=>{
+        console.log(`notes Server lisening ${port}`);
+    });
+}).catch(err => {
+    console.log('Something went wrong');
+    console.log(err);
 });
