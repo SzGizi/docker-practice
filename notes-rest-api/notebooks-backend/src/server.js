f4c24801b4a77bfd200ca7eaf59bce68fa3e4219
Gizi <szanto.gizela@gmail.com>
2025-10-15 11:04:44 +0200

    Setup Docker Compose for Notebooks Services

--- notes-rest-api/notebooks-backend/src/server.js
+++ notes-rest-api/notebooks-backend/src/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.get('/',(req,res)=> res.json({message: 'hello from notebooks'}));
 
 const port = process.env.PORT;
-app.listen(port,()=>{
-    console.log(`notebooks Server lisening ${port}`);
+
+mongoose.connect(process.env.DB_URL).then(()=>{
+    console.log("Connected to MOngoDB! Start server");
+
+    app.listen(port,()=>{
+        console.log(`notebooks Server lisening ${port}`);
+    });
+}).catch(err => {
+    console.log('Something went wrong');
+    console.log(err);
 });
